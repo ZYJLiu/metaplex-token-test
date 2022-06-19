9db5ced47574446937a41834282caac06cbf7696
john <johnzhengyuliu@gmail.com>
2022-06-19 09:55:05 -0500

    fix

--- src/views/uploader/index.tsx
+++ src/views/uploader/index.tsx
@@ -1,19 +1,15 @@
-import { FC } from 'react';
+import { FC } from "react";
 
-import { UploadMetadata } from "components/TestUploadMetaData";
+// import { UploadMetadata } from "components/TestUploadMetaData";
 
-export const UploaderView: FC = ({ }) => {
-
+export const UploaderView: FC = ({}) => {
   return (
-
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
           Upload Metadata
-        </h1>      
-        <div className="text-center">
-          <UploadMetadata />
-        </div>
+        </h1>
+        <div className="text-center">{/* <UploadMetadata /> */}</div>
       </div>
     </div>
   );
